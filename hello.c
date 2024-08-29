d836948300a52b96d5c75f2d8f923527a14b831e
Colter <cvbird26@gmail.com>
2024-08-29 12:55:46 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Colter Musgrove
+ *         cmusgrove3@huskers.unl.edu
+ * Date: 8/27/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Colter Musgrove\n");
+
+    return 0;
+}
